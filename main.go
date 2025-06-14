e023588223562cbf548d0c1b7b47916a1a8096f6
Rasmus Secher <rse@embankment.com>
2025-06-14 12:36:51 +0200

    CH4L2

--- main.go
+++ main.go
@@ -1,12 +1,134 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("increment")
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
 
 func main() {
-	serveMux := http.NewServeMux()
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
+	mux := http.NewServeMux()
+
+	// Routes
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
+	mux.HandleFunc("GET /admin/metrics", apiCfg.fileserverHitsHandler)
+	mux.HandleFunc("POST /admin/reset", apiCfg.fileserverResetHandler)
+	mux.HandleFunc("GET /api/healthz", handlehealthz)
+	mux.HandleFunc("POST /api/validate_chirp", validateChirpHandler)
+
+	// Server
 	server := http.Server{
-		Handler: serveMux,
+		Handler: mux,
 		Addr:    ":8080",
 	}
+	fmt.Printf("Listening on localhost%s 🚀\n", server.Addr)
 	server.ListenAndServe()
+}
+
+func handlehealthz(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) fileserverHitsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	htmlTemplate := fmt.Sprintf(`<!DOCTYPE html>
+<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+  </body>
+</html>`, cfg.fileserverHits.Load())
+	w.Write([]byte(htmlTemplate))
+}
+
+func (cfg *apiConfig) fileserverResetHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("reset")
+	cfg.fileserverHits.Store(0)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Reset: %d", cfg.fileserverHits.Load())))
+}
+
+func validateChirpHandler(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
+	}
+	type returnError struct {
+		Msg string `json:"error"`
+	}
+	type returnSuccess struct {
+		Msg bool `json:"valid"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+
+	// Handle erorr in JSON decode
+	if err != nil {
+		genericError := returnError{
+			Msg: "Something went wrong",
+		}
+		dat, err := json.Marshal(genericError)
+		if err != nil {
+			log.Printf("Error marshalling JSON: %s", err)
+			w.WriteHeader(500)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(500)
+		w.Write(dat)
+		return
+	}
+
+	// Handle Chirp length error
+	if len(params.Body) > 140 {
+		lengthError := returnError{
+			Msg: "Chirp is too long",
+		}
+		dat, err := json.Marshal(lengthError)
+		if err != nil {
+			log.Printf("Error marshalling JSON: %s", err)
+			w.WriteHeader(500)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(400)
+		w.Write(dat)
+		return
+	}
+
+	// Handle succesful Chirp
+	success := returnSuccess{
+		Msg: true,
+	}
+	dat, err := json.Marshal(success)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	w.Write(dat)
 }
